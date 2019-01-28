328dd9ba13c739a5c1d68adf55c3c8e96330e404
Paul Blouët <paul.blouet@ajeel.fr>
2019-01-28 16:34:13 +0100

    PHP Client basics

---
+++ clientphp/assets/js/app.ts
@@ -0,0 +1,5 @@
+import '../css/app.css';
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
---
+++ clientphp/webpack.config.js
@@ -0,0 +1,15 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.ts')
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .enableTypeScriptLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
